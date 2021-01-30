89f32c7df13e0103033c5b87e90705e143ec67f7
anu-bhav <myster.anubhav@gmail.com>
2021-01-30 10:18:10 +0400

    added cv and photo

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-// import Pdf from "../../editable-stuff/resume.pdf";
+import React, { useState /*, useEffect */ } from "react";
+// import axios from "axios";
+import Pdf from "../../editable-stuff/resume.pdf";
 import {
   aboutHeading,
   aboutDescription,
   showInstaProfilePic,
-  instaLink,
-  instaUsername,
-  instaQuerry,
+  // instaLink,
+  // instaUsername,
+  // instaQuerry,
 } from "../../editable-stuff/configurations.json";
 
 const AboutMe = () => {
-  const [instaProfilePic, setInstaProfilePic] = useState("");
-  const [showInsta, setShowInsta] = useState(showInstaProfilePic);
-  // const [resumeURL] = useState(Pdf);
+  // const [instaProfilePic, setInstaProfilePic] = useState("");
+  // const [showInsta, setShowInsta] = useState(showInstaProfilePic);
+  const [showInsta] = useState(showInstaProfilePic);
+  const [resumeURL] = useState(Pdf);
 
-  useEffect(() => {
-    if (showInsta) {
-      handleRequest();
-    }
-  }, [showInsta]);
+  // useEffect(() => {
+  //   if (showInsta) {
+  //     handleRequest();
+  //   }
+  // }, [showInsta]);
 
-  const handleRequest = (e) => {
-    axios
-      .get(instaLink + instaUsername + instaQuerry)
-      .then((response) => {
-        // handle success
-        // console.log(response.data.graphql);
-        return setInstaProfilePic(
-          response.data.graphql.user.profile_pic_url_hd
-        );
-      })
-      .catch((error) => {
-        // handle error
-        setShowInsta(false);
-        return console.error(error.message);
-      })
-      .finally(() => {
-        // always executed
-      });
-  };
+  // const handleRequest = (e) => {
+  //   axios
+  //     .get(instaLink + instaUsername + instaQuerry)
+  //     .then((response) => {
+  //       // handle success
+  //       // console.log(response.data.graphql);
+  //       return setInstaProfilePic(
+  //         response.data.graphql.user.profile_pic_url_hd
+  //       );
+  //     })
+  //     .catch((error) => {
+  //       // handle error
+  //       setShowInsta(false);
+  //       return console.error(error.message);
+  //     })
+  //     .finally(() => {
+  //       // always executed
+  //     });
+  // };
 
   return (
     <div id="aboutme" className="jumbotron jumbotron-fluid bg-transparent m-0">
@@ -49,15 +50,16 @@ const AboutMe = () => {
             <div className="col-5 d-none d-lg-block align-self-center">
               <img
                 className="border border-secondary rounded-circle"
-                src={instaProfilePic}
+                src={require("../../editable-stuff/images/me.png")}
                 alt="profilepicture"
+                style={{ width: "100%", height: "100%" }}
               />
             </div>
           )}
           <div className={`col-lg-${showInsta ? "7" : "12"}`}>
             <h1 className="display-4 mb-5 text-center">{aboutHeading}</h1>
             <p className="lead text-center">{aboutDescription}</p>
-            {/* {resumeURL && (
+            {resumeURL && (
               <p className="lead text-center">
                 <a
                   className="btn btn-outline-dark btn-lg"
@@ -70,7 +72,7 @@ const AboutMe = () => {
                   Resume
                 </a>
               </p>
-            )} */}
+            )}
           </div>
         </div>
       </div>
